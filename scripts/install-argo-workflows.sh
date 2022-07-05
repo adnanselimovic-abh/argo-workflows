7c0c54a325d7d7523e6ba9e7314fbb2c8ae63395
extern-selimovic-adnan <extern.selimovic_adnan@allianz.de>
2022-07-05 16:01:25 +0200

    Deploy to argo namespace.

--- scripts/install-argo-workflows.sh
+++ scripts/install-argo-workflows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 kubectl create ns argo
-helm install atlantbh charts/argo-workflows --values charts/argo-values.yaml
+helm install atlantbh charts/argo-workflows --namespace argo --values charts/argo-values.yaml
 
 kubectl apply -f resources/raw/yaml/ingresses.yaml
--- scripts/setup-k3s.sh
+++ scripts/setup-k3s.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp resources/raw/yaml/traefik-config-k3s.yaml /var/lib/rancher/k3s/server/manifests/traefik-config.yaml
+cp scripts/resources/raw/yaml/traefik-config-k3s.yaml /var/lib/rancher/k3s/server/manifests/traefik-config.yaml
