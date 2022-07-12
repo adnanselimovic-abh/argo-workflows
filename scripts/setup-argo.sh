3c14bd9fed7a4eb0cbb54af1d1f7162311050768
extern-selimovic-adnan <extern.selimovic_adnan@allianz.de>
2022-07-12 10:58:16 +0200

    Fix setup-argo.sh to run by root.

--- scripts/setup-argo.sh
+++ scripts/setup-argo.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-alias k=kubectl
 
-k create ns argo
-k create ns argo-events
+kubectl create ns argo
+kubectl create ns argo-events
 
-helm upgrade --install atlantbh charts/argo-workflows --namespace argo --values charts/argo-values.yaml
-helm upgrade --install atlantbh charts/argo-events --namespace argo-events --values charts/argo-values.yaml
+helm upgrade --install atlantbh charts/argo-workflows --namespace argo --values charts/argo-workflows/argo-values.yaml
+helm upgrade --install atlantbh charts/argo-events --namespace argo-events --values charts/argo-events/argo-values.yaml
 
-k apply -f scripts/resources/raw/yaml/setup/ingresses.yaml
-k apply -f scripts/resources/raw/yaml/argo-workflows/service-account-workflow.yaml
-k apply -f scripts/resources/raw/yaml/argo-events/service-account-events.yaml
-k apply -f scripts/resources/raw/yaml/argo-events/webhook-source.yaml
-k apply -f scripts/resources/raw/yaml/argo-events/eventbus-default.yaml
-k apply -f scripts/resources/raw/yaml/argo-events/webhook-sensor.yaml
+kubectl apply -f scripts/resources/raw/yaml/setup/ingresses.yaml
+kubectl apply -f scripts/resources/raw/yaml/argo-workflows/service-account-workflow.yaml
+kubectl apply -f scripts/resources/raw/yaml/argo-events/service-account-events.yaml
+kubectl apply -f scripts/resources/raw/yaml/argo-events/webhook-source.yaml
+kubectl apply -f scripts/resources/raw/yaml/argo-events/eventbus-default.yaml
+kubectl apply -f scripts/resources/raw/yaml/argo-events/webhook-sensor.yaml
 
